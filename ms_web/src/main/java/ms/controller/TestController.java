bbe8e2ff9e26dcfb6aceb3b385adf7718d37d735
jankin y win <yjq1471822308@163.com>
2023-04-18 13:33:38 +0800

    springboot

---
+++ ms_web/src/main/java/ms/controller/TestController.java
@@ -0,0 +1,12 @@
+package ms.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping("/test")
+    private String test(){
+        return "奥里给";
+    }
+}
