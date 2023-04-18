867cd5cbc62b539d001dd42145694b14da8c5127
jankin y win <yjq1471822308@163.com>
2023-04-18 16:36:51 +0800

    日志

--- ms_web/src/main/java/ms/controller/TestController.java
+++ ms_web/src/main/java/ms/controller/TestController.java
@@ -1,12 +1,14 @@
 package ms.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@Slf4j
 @RestController
 public class TestController {
     @RequestMapping("/test")
     private String test(){
+        log.info("日志");
         return "奥里给";
     }
 }
